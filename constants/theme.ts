be7f3bb96c4fffd99c4b172265df822bb5e92750
MichieCodes <49980016+MichieCodes@users.noreply.github.com>
2022-03-27 14:11:08 -0500

    Add constant colors, sizes, and styles

---
+++ constants/theme.ts
@@ -0,0 +1,32 @@
+export const COLORS = {
+  white: '#FFF',
+  black: '#000',
+
+  text_light: '#F0F0F5',
+
+  darkest_blue: '#151521',
+  dark_blue: '#2D2D47',
+  light_blue: '#7E7EAC',
+
+  red: '#FF5F56',
+  yellow: '#FFBD2E',
+  green: '#27C93F',
+
+  transparent: 'transparent',
+  transparent_darkest_blue: 'rgba(21, 21, 33, 0.5)',
+
+  background_gradient: 'radial-gradient(50% 50% at 50% 50%, rgba(45, 45, 71, 0.9) 0%, #2D2D47 100%)',
+  button_gradient: 'linear-gradient(to bottom, #7472C5 0%, #4A48A0 100%)',
+};
+
+export const SIZES = {
+  s_xs: '0.6rem',
+  s_sm: '1.5rem',
+  s_md: '2rem',
+  s_lg: '3rem',
+  s_xl: '4rem',
+};
+
+export const STYLES = {
+  shadow: '0px 0px 4px 2px rgba(0, 0, 0, 0.25), 0px 0px 20px rgba(0, 0, 0, 0.5)',
+}
