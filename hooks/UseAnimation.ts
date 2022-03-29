bc78391c3fb709451adf2018abdc697e8cb4bd35
MichieCodes <49980016+MichieCodes@users.noreply.github.com>
2022-03-29 03:26:52 -0500

    Implement useAnimation hook

---
+++ hooks/UseAnimation.ts
@@ -0,0 +1,48 @@
+import React from 'react'
+import {Keyframes} from 'styled-components';
+
+export function useAnimation<T>(
+  enterAnimation : Keyframes,
+  leaveAnimation ?: Keyframes
+) {
+  const element = React.useRef<T>(null)
+  const [completed, setCompleted] = React.useState<string|undefined>()
+  const [animation, setAnimation] = React.useState<Keyframes|undefined>()
+
+  const enter = React.useCallback(() => {
+    setAnimation(enterAnimation)
+  }, [enterAnimation])
+
+  const leave = React.useCallback(() => {
+    setAnimation(leaveAnimation)
+  }, [leaveAnimation])
+
+  const toggle = React.useCallback(() => {
+    setAnimation((currentAnimation) => 
+      (!currentAnimation || currentAnimation === leaveAnimation)
+        ? enterAnimation
+        : leaveAnimation
+    )
+  }, [enterAnimation, leaveAnimation])
+
+  React.useEffect(() => {
+    if(!element.current) return
+
+    const onAnimationEnd = (e : AnimationEvent) => {
+      setCompleted(e.animationName)
+    }
+
+    element.current.addEventListener(
+      'animationend',
+      onAnimationEnd
+    )
+
+    return () => 
+      element.current?.removeEventListener(
+        'animationend',
+        onAnimationEnd
+      )
+  }, [element])
+
+  return {element, animation, completed, enter, leave, toggle}
+}
