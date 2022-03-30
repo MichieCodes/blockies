37fd658626dae2055fd02be09bf9c9bb638ce67b
MichieCodes <49980016+MichieCodes@users.noreply.github.com>
2022-03-30 03:35:02 -0500

    Install react code editor

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+const removeImports = require('next-remove-imports')()
+
 const nextConfig = {
+  ...removeImports({
+    experimental: {
+      esmExternals: true
+    }
+  }),
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
-  },
+  }
 }
 
 module.exports = nextConfig
